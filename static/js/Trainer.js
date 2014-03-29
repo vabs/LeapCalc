2e3249c12a263e11d728c334daccf57508c0a7b8
Vaibhav Somani <njoy.today@gmail.com>
2014-03-29 02:49:52 -0400

    code added for getures

--- static/js/Trainer.js
+++ static/js/Trainer.js
@@ -3,12 +3,26 @@
  */
 var leapController = new Leap.Controller();
 
-var trainer = new LeapTrainer.Controller({controller: leapController, trainingCountdown: 3, trainingGestures: 3});
+var trainer = new LeapTrainer.Controller({controller: leapController,
+                                          trainingCountdown: 3,
+                                          trainingGestures: 3,
+                                          minRecordingVelocity: 100,
+                                          downtime: 2000});
 
 leapController.connect();
 
-trainer.create('add');
-trainer.on('add', function(){
-    console.log('training is complete');
-    console.log(trainer.toJSON('add'));
+//trainer.create('add');
+trainer.create('minus');
+
+trainer.on('training-complete', function(name){
+    console.log('training is complete!' + name);
+});
+
+trainer.on('gesture-recognized', function(hit, name){
+    console.log(hit, name);
+});
+trainer.on('minus', function(){
+    console.log('minus function');
+    //console.log(trainer.toJSON('add'));
+
 });
