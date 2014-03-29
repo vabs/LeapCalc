ec2d157f3aa845920e3e585ce3c51478d14a725f
Vaibhav Somani <njoy.today@gmail.com>
2014-03-28 23:28:47 -0400

    added finger detection

--- LeapCalc.py
+++ LeapCalc.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('Home.html')
 
+
+@app.route('/train')
+def train():
+    return render_template('Trainer.html')
 
 if __name__ == '__main__':
     app.run()
